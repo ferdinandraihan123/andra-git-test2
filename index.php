6b9702f3e92e0b30cd9be032b933f36031ad0d17
ferdinandraihan123 <andraferdinand1234@gmail.com>
2024-12-24 06:27:11 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>git temote</title>
 </head>
+
 <body>
     <h1>test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos sapiente accusamus facere neque corporis
+        vitae eligendi nisi amet fugiat, ab facilis ad doloribus fugit, sit magni, aliquam illum saepe labore!</p>
 </body>
+
 </html>
